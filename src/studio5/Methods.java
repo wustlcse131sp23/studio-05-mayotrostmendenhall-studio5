b0c57eaee6b1564b478e46f4a3a698f9f8f9cb00
Brendan <Brendan@DESKTOP-GFGPGU6.dhcp.wustl.edu>
2023-03-02 12:42:49 -0600

    done

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -1,5 +1,7 @@
 package studio5;
 
+import java.lang.reflect.Array;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Methods {
@@ -16,9 +18,11 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
 		return distance;
+		
 	}
+	
 
 	/**
 	 * Draw a bull's eye at the given location with the given radius.
@@ -34,17 +38,20 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
+		StdDraw.setPenColor(0, 109, 219);
+		StdDraw.filledCircle(x, y, 3.0/4.0*(radius));
 		
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(146, 0, 0);
+		StdDraw.filledCircle(x, y, 1.0/2.0*(radius));
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(255, 255, 109);
+		StdDraw.filledCircle(x, y, 1.0/4.0*(radius));
 		
 	}
 
@@ -63,6 +70,18 @@ public class Methods {
 		String result = "";
 		// TODO: Finish this method
 		
+		for(int i = 0; i < source.length(); i++){
+			if(source.charAt(i)==(target))
+			{
+				result= result + replacement;
+			}
+			else
+			{
+				result= result + source.charAt(i);
+			}
+				
+				
+		}
 		return result;
 	}
 
